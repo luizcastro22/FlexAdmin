3ba81d02b829ab4a05783a654a87c59d11e65e45
luizcastro22 <143292928+luizcastro22@users.noreply.github.com>
2023-08-26 23:48:38 +0000

    Criado estrutura de login e usuarios

--- projeto_academia/settings.py
+++ projeto_academia/settings.py
@@ -100,6 +100,9 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+#redireciona apos login
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
 
 
 # Internationalization
